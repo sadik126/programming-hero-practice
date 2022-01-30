bf1d7eac7e21676fb513ae09c96a95fe6432dbb5
sadik126 <sadikhimel04@gmail.com>
2022-01-30 20:22:09 +0600

    ms

---
+++ practice_js.js
@@ -0,0 +1,37 @@
+inch to feet 
+
+function inchtofeet(inch){
+    var feet = inch*12;
+    return feet;
+}
+
+ var answer = inchtofeet(156);
+ console.log(answer);
+
+
+ //miles to km 
+function milestokm(mile){
+    var miles = mile*1.609344;
+    return miles;
+}
+
+var answer = milestokm(23);
+console.log(answer);
+
+
+
+function evencheck(num){
+ if(num%2==0){
+    //  return true;
+     console.log('this is even');
+ }
+ else{
+     return ('this is odd') ;
+    //  console.log('this is odd');
+ }
+}
+
+var number = 23;
+var result = evencheck(number);
+console.log(result);
+
