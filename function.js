d440d8b23cdf174768cc77b0759c77a7d926bc89
sadik126 <sadikhimel04@gmail.com>
2022-01-31 18:54:25 +0600

    ms

--- function.js
+++ function.js
@@ -5,3 +5,31 @@ return sum;
 
  var result = addtwonumbers(2,3);
  console.log('the result is ', result);
+
+
+
+//  display custom message 
+
+function custommsg(name){
+ return ('hello '+name);
+}
+
+var msg = custommsg('sadik');
+console.log('she said ', msg);
+
+
+//pizza delivery 
+
+function pizza(name,quantity){
+if(name == 'pizza'){
+    var total = 10 * quantity;
+    return('hey '+ name + 'here is your '+ quantity+'pizza. your total price is '+total)
+}
+else {
+    return('we only sells pizza.sorry');
+}
+ 
+}
+
+var msg = pizza('pizza',3);
+console.log(msg);
