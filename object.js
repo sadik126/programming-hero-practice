60fd0d556ce2d6e687a86836d13b0157cc88a3dd
sadik126 <sadikhimel04@gmail.com>
2022-01-31 19:44:41 +0600

    ms

---
+++ object.js
@@ -0,0 +1,12 @@
+ var hero = {
+     name:'sadik',
+     age: 25,
+     status: 'married',
+     myluckynum : [8,999,6565,545421,55],
+ }
+
+ var result = hero.myluckynum[4];
+ console.log(result);
+
+
+ 
