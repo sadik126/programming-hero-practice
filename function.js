1b0e3dd06f75c9efbcab5ea9d37c68d35c1d2fa2
sadik126 <sadikhimel04@gmail.com>
2022-01-31 16:52:24 +0600

    ms

---
+++ function.js
@@ -0,0 +1,7 @@
+function addtwonumbers(num1 , num2 ){
+sum = num1+num2;
+return sum;
+}
+
+ var result = addtwonumbers(2,3);
+ console.log('the result is ', result);
--- practice_js.js
+++ practice_js.js
@@ -23,7 +23,7 @@ console.log(answer);
 function evencheck(num){
  if(num%2==0){
     //  return true;
-     console.log('this is even');
+     return('this is even');
  }
  else{
      return ('this is odd') ;
@@ -31,7 +31,18 @@ function evencheck(num){
  }
 }
 
-var number = 23;
+var number = 22;
 var result = evencheck(number);
 console.log(result);
+
+
+// factorial inserts
+
+var factorial = 1 ;
+ for (let index = 1; index <=7; index++) {
+     factorial = factorial*index;
+     
+ }
+
+ console.log(factorial);
 
