5aadde5a41d235f4fa9c0b70246cc1cb2e6c2d3a
sadik126 <sadikhimel04@gmail.com>
2022-02-01 19:16:01 +0600

    ms

---
+++ array.js
@@ -0,0 +1,45 @@
+// var array = [10,20,30,40,50];
+// var sum1 = 0;
+
+// for (let index = 0; index <=4; index++) {
+  
+//     sum1 = sum1 + array[index];
+//     console.log(sum1);
+
+//     //ekhane shob shongkar sathe shob songkha jog hbe
+
+   
+// }
+
+// console.log(sum1);
+// //ekhane total array jog hbe
+
+
+
+//find the largest number
+let largest = 0;
+function largestnumber(numbers){
+for (let index = 0; index < numbers.length; index++) {
+    let element = numbers[index];
+    
+    if(element>largest)
+    {
+        largest = element;
+    }
+    
+}
+return largest;
+}
+
+var numbers = [21,35,65,45,85,49];
+var oldest = largestnumber(numbers);
+console.log(oldest);
+
+
+
+//1+2+4+6+8
+function series(numbers){
+    
+}
+
+
---
+++ switchcase.js
@@ -0,0 +1,12 @@
+var number = 40;
+
+switch(number){
+    case 40:
+        console.log("you are men");
+        break;
+    case 80:
+        console.log("you are young");
+
+    default:
+            console.log("you are hero");
+}
