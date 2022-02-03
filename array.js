eebcc677f40f429b62b989a4e7903c275a1f13c5
sadik126 <sadikhimel04@gmail.com>
2022-02-03 21:49:07 +0600

    ms

--- array.js
+++ array.js
@@ -1,12 +1,26 @@
-var array = [10, 20, 30, 40, 50];
-var sum1 = 0;
+// var array = [10, 20, 30, 40, 50];
+// var sum1 = 0;
 
-for (let index = 0; index <= 4; index++) {
-  sum1 = sum1 + array[index];
-  console.log(sum1);
+// for (let index = 0; index <= 4; index++) {
+//   sum1 = sum1 + array[index];
+//   console.log(sum1);
 
-  //ekhane shob shongkar sathe shob songkha jog hbe
+//   //ekhane shob shongkar sathe shob songkha jog hbe
+// }
+
+// console.log(sum1);
+
+//ekhane total array jog hbe
+
+
+var array = [1,25,35,45,56];
+
+let sum = 0;
+
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+  sum = sum+element;
+  
 }
 
-console.log(sum1);
-//ekhane total array jog hbe
+console.log('sum is ',sum);
