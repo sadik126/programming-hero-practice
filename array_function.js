aa43ec21c63dc9fd6933eda052032b9800db1e90
sadik126 <sadikhimel04@gmail.com>
2022-02-03 22:09:09 +0600

    ms

--- array_function.js
+++ array_function.js
@@ -3,6 +3,7 @@ var sum = 0;
 for (let index = 0; index < array.length; index++) {
     let element = array[index];
    var sum = sum + element;
+     console.log(element);
    
     
 }
@@ -10,6 +11,7 @@ return sum;
 
 }
 
-var array = [23,35,45,75,84];
+// var array = [23,35,45,75,84];
+let array = new Array(1,25,56,45,48);
 var total = totalnumber(array);
 console.log('sum is ', total);
