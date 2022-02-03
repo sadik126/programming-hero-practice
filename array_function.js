149a9de43129acf78e16cb75208b2c270a62e72b
sadik126 <sadikhimel04@gmail.com>
2022-02-03 22:10:51 +0600

    ms

--- array_function.js
+++ array_function.js
@@ -1,6 +1,8 @@
 function totalnumber(array){
 var sum = 0;
 for (let index = 0; index < array.length; index++) {
+    //array te ja ja  element ase oigula show hbe
+    
     let element = array[index];
    var sum = sum + element;
      console.log(element);
@@ -13,5 +15,7 @@ return sum;
 
 // var array = [23,35,45,75,84];
 let array = new Array(1,25,56,45,48);
+//add array element
+array[5] = 100;
 var total = totalnumber(array);
 console.log('sum is ', total);
