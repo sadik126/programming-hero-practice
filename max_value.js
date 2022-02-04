6378f9c1432e466f5fe6a9038d7073b317164e0f
sadik126 <sadikhimel04@gmail.com>
2022-02-04 19:54:09 +0600

    ms

---
+++ max_value.js
@@ -0,0 +1,16 @@
+var sakib = 450;
+var tamim = 650;
+var mushfiq = 250;
+
+if (sakib>tamim && sakib>mushfiq) {
+    console.log('sakib is the best');
+} 
+else if (tamim>sakib && tamim>mushfiq){
+   console.log('tamim is the best');
+}
+else{
+    console.log('musfiqur rahim is best')
+}
+    
+
+
---
+++ swap.js
@@ -0,0 +1,7 @@
+var first = 15;
+var second = 16;
+
+[first,second]=[second,first];
+
+console.log(first);
+console.log(second);
