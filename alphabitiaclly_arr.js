cd058733d414144a91b3bde7309732b5a09dddc1
sadik126 <sadikhimel04@gmail.com>
2022-02-06 19:43:15 +0600

    m

--- alphabitiaclly_arr.js
+++ alphabitiaclly_arr.js
@@ -20,3 +20,15 @@ function result2(){
 var number = [3,5,8,14,1,4];
 var number1 = result2(number);
 console.log(number1);
+
+
+
+
+const array = [ 2, 3, 4, 5, 6, 7];
+
+for (let i = 1; i < array.length; i++) {
+ if (i == 5) {
+  continue;
+ }
+ console.log(array[i]);
+}
