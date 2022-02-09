bb107d4ed154d5bce902746c2cd7402899374085
sadik126 <sadikhimel04@gmail.com>
2022-02-09 19:14:48 +0600

    m

--- comment.js
+++ comment.js
@@ -1,3 +1,8 @@
 document.getElementById('submit').addEventListener('click',function(){
 let comment = document.getElementById('new-comment');
+
+const usercomment = comment.value;
+
+let p = document.createElement('p');
+p.innerText = usercomment;
 });
