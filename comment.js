f207aea99ee426e67e9c29bf1465185800e5f225
sadik126 <sadikhimel04@gmail.com>
2022-02-09 20:24:22 +0600

    ms

--- comment.js
+++ comment.js
@@ -4,7 +4,20 @@ document.getElementById('submit').addEventListener('click',function(){
     let comment = document.getElementById('new-comment');
 
 const usercomment = comment.value;
+
 //create comment paragraph
 let p = document.createElement('p');
 p.innerText = usercomment;
+
+
+let addcomment = document.getElementById('comment');
+addcomment.appendChild(p);
+ comment.value = '';
+
+
+ document.getElementById('delete').addEventListener('click',function(){
+    addcomment.style.display='none';
+ })
+
+
 });
