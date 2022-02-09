0606e20719f75ed72a52fc3d253dabd02ea300eb
sadik126 <sadikhimel04@gmail.com>
2022-02-09 19:20:14 +0600

    m

--- comment.js
+++ comment.js
@@ -1,8 +1,10 @@
+//get submit button
 document.getElementById('submit').addEventListener('click',function(){
-let comment = document.getElementById('new-comment');
+//get input text from
+    let comment = document.getElementById('new-comment');
 
 const usercomment = comment.value;
-
+//create comment paragraph
 let p = document.createElement('p');
 p.innerText = usercomment;
 });
