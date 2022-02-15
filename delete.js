74e003a78577c73068401e5566a31ff4cd24bee2
sadik126 <sadikhimel04@gmail.com>
2022-02-15 20:59:01 +0600

    m

--- delete.js
+++ delete.js
@@ -1,3 +1,12 @@
 document.getElementById('dltbtn').addEventListener('click',function(){
-    console.log('deleted');
+   document.getElementById('delete').style.display = 'none';
 })
+
+
+document.getElementById('delete-confirm').addEventListener('focus',function(){
+    document.getElementById('delete-confirm').style.borderBlockColor='black';
+ })
+
+ document.getElementById('delete-confirm').addEventListener('blur',function(){
+    document.getElementById('delete-confirm').style.borderBlockColor='red';
+ })
