93afde791d495d5eb34a303359b2ca2e8fb8c3b8
sadik126 <sadikhimel04@gmail.com>
2022-02-22 18:26:53 +0600

    ms

--- delete.js
+++ delete.js
@@ -10,3 +10,22 @@ document.getElementById('delete-confirm').addEventListener('focus',function(){
  document.getElementById('delete-confirm').addEventListener('blur',function(){
     document.getElementById('delete-confirm').style.borderBlockColor='red';
  })
+
+ document.getElementById('delete-confirm').addEventListener('keyup',function(event){
+  let valuebox =  document.getElementById('delete-confirm');
+  console.log(valuebox.value);
+  if(event.target.value == 'delete'){
+    document.getElementById('dltbtn').removeAttribute('disabled');
+  }
+  else{
+    document.getElementById('dltbtn').setAttribute('disabled',true);
+  }
+
+ })
+
+
+//  document.getElementById('delete-confirm').addEventListener('change',function(e){
+//     let valuebox =  document.getElementById('delete-confirm');
+//     console.log(valuebox.value);
+  
+//    })
