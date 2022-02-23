21aa5ed74e3c7b20d24bf1bbcb476af54bb32700
sadik126 <sadikhimel04@gmail.com>
2022-02-23 20:48:56 +0600

    ms

--- country.js
+++ country.js
@@ -1,1 +1,22 @@
 console.log('hello country')
+
+let loadcountry = () => {
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => displaycountries(data))
+}
+
+loadcountry();
+
+
+
+const displaycountries = (countries) => {
+   
+    // for (const country of countries) {
+    //     console.log(country)
+    // }
+
+    countries.forEach(country => {
+        console.log('here is ' , country);
+    });
+}
