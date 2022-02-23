ba674ed68684a79f4a9993b55f192a70bf74374c
sadik126 <sadikhimel04@gmail.com>
2022-02-23 19:38:59 +0600

    m

--- displayapi.js
+++ displayapi.js
@@ -11,5 +11,15 @@ function displaypost(data){
      console.log(post.title)
      const section = document.getElementById('container');
      const div = document.createElement('div');
+     div.classList.add('post');
+    
+     div.innerHTML = 
+     `
+     <h3>${post.title}</h3>
+     <p>${post.body}</p>
+     
+     `
+
+     section.appendChild(div);
  }
 }
