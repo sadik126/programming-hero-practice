d8205f7f20b0630fba4cc005abe58a37e9748a5a
sadik126 <sadikhimel04@gmail.com>
2022-04-09 17:36:26 +0600

    m

--- sign-in/src/App.js
+++ sign-in/src/App.js
@@ -1,9 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { getAuth } from "firebase/auth";
+import app from './Firebase.init';
+
+const auth = getAuth(app)
 
 function App() {
   return (
     <div className="App">
+      <form>
+        <input type="text" name="" id="" />
+        <input type="password" name="" id="" />
+      </form>
 
     </div>
   );
