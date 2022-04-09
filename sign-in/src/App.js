304f3d83ae355bb321d22de0691520fbd71660b4
sadik126 <sadikhimel04@gmail.com>
2022-04-09 18:43:22 +0600

    m

--- sign-in/src/App.js
+++ sign-in/src/App.js
@@ -1,19 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { getAuth } from "firebase/auth";
 import app from './Firebase.init';
 
 const auth = getAuth(app)
 
 function App() {
-  const email = () => {
+  const email = (e) => {
+    console.log(e.target.value)
+  }
 
+  const password = (e) => {
+    console.log(e.target.value)
+  }
+  const submit = (e) => {
+    console.log('submitted')
+    e.preventDefault();
   }
   return (
     <div className="App">
-      <form>
-        <input onChange={email} type="email" name="" id="" />
-        <input type="password" name="" id="" />
+      <form onSubmit={submit}>
+        <input onChange={email} type="email" name="" id="" /><br />
+        <input onChange={password} type="password" name="" id="" /><br />
+        <input type="submit" value="login" />
       </form>
 
     </div>
