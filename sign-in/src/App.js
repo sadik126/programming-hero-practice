f462b8c240cb991a0cd4d8ea844b3a9da7c0822a
sadik126 <sadikhimel04@gmail.com>
2022-04-09 21:03:29 +0600

    m

--- sign-in/src/App.js
+++ sign-in/src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
+import Button from 'react-bootstrap/Button';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from 'react-bootstrap/Form'
 import { getAuth } from "firebase/auth";
 import app from './Firebase.init';
 
@@ -20,11 +22,28 @@ function App() {
   }
   return (
     <div className="App">
-      <form onSubmit={submit}>
-        <input onChange={email} type="email" name="" id="" /><br />
-        <input onChange={password} type="password" name="" id="" /><br />
-        <input type="submit" value="login" />
-      </form>
+      <div className="regestration">
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
 
     </div>
   );
