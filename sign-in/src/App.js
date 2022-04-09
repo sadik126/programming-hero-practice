bcf0926c98241e7586101488dd9b5017b32e1d73
sadik126 <sadikhimel04@gmail.com>
2022-04-09 17:51:07 +0600

    m

--- sign-in/src/App.js
+++ sign-in/src/App.js
@@ -6,10 +6,13 @@ import app from './Firebase.init';
 const auth = getAuth(app)
 
 function App() {
+  const email = () => {
+
+  }
   return (
     <div className="App">
       <form>
-        <input type="text" name="" id="" />
+        <input onChange={email} type="email" name="" id="" />
         <input type="password" name="" id="" />
       </form>
 
