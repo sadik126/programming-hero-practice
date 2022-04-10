414310a4f2113112a1606761d01b9c0e2d0a8f02
sadik126 <sadikhimel04@gmail.com>
2022-04-11 05:15:51 +0600

    m

--- sign-in/src/App.js
+++ sign-in/src/App.js
@@ -5,14 +5,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Form from 'react-bootstrap/Form'
 import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import app from './Firebase.init';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const auth = getAuth(app)
 
 function App() {
 
-  const [emailid, setEmailid] = useEffect('');
-  const [pass, setPass] = useEffect('');
+  const [emailid, setEmailid] = useState('');
+  const [pass, setPass] = useState('');
+  const [error, setError] = useState('');
+  const [validated, setValidated] = useState(false);
   const email = (e) => {
     setEmailid(e.target.value)
   }
@@ -20,39 +22,69 @@ function App() {
   const password = (e) => {
     setPass(e.target.value)
   }
-  const submit = (e) => {
+  const submit = (event) => {
+
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+
+      event.stopPropagation();
+      return;
+    }
+
+
+    if (!/(?=.*[a-zA-Z >>!#$%&? "<<])[a-zA-Z0-9 >>!#$%&?<< ]/.test(pass)) {
+      setError('password should contain 1 charecter');
+      return
+    }
+
+    setValidated(true);
+    setError('');
 
     createUserWithEmailAndPassword(auth, emailid, pass)
       .then(result => {
         const user = result.user;
         console.log(user)
+        setEmailid('')
+        setPass('')
       })
       .catch(error => {
         console.error(error)
+        setError(error.message)
       })
 
-    e.preventDefault();
+    event.preventDefault();
   }
+
+
   return (
     <div >
       <div className="regestration w-50 mx-auto mt-5">
         <h2 className='text-success'>Please register!!</h2>
-        <Form onSubmit={submit}>
+        <Form noValidate validated={validated} onSubmit={submit}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control onChange={email} type="email" placeholder="Enter email" />
+            <Form.Control onChange={email} type="email" placeholder="Enter email" required />
             <Form.Text className="text-muted">
               We'll never share your email with anyone else.
             </Form.Text>
+            <Form.Control.Feedback type="invalid">
+              Please choose a email.
+            </Form.Control.Feedback>
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control onChange={password} type="password" placeholder="Password" />
+            <Form.Control onChange={password} type="password" placeholder="Password" required />
+            <Form.Control.Feedback type="invalid">
+              Please choose a password.
+            </Form.Control.Feedback>
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicCheckbox">
             <Form.Check type="checkbox" label="Check me out" />
           </Form.Group>
+          <p className='text-danger'>{error}</p>
           <Button variant="primary" type="submit">
             Submit
           </Button>
