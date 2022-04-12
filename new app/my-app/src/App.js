5baa5b738730f2f67b45713642b2bc1327eebad9
sadik126 <sadikhimel04@gmail.com>
2022-04-12 20:28:49 +0600

    m

--- new app/my-app/src/App.js
+++ new app/my-app/src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Components/Home/Home';
+import Login from './Components/Login/Login';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/login' element={<Home></Home>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
       </Routes>
     </div>
   );
