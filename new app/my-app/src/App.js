52606ef225fda4ab1abf34e3ead4880a2cdb7e5b
sadik126 <sadikhimel04@gmail.com>
2022-04-12 20:17:24 +0600

    m

--- new app/my-app/src/App.js
+++ new app/my-app/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route>
 
+        </Route>
+      </Routes>
     </div>
   );
 }
