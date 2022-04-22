0636f8ba888fa1a07d8d895c422471a4a38ff260
sadik126 <sadikhimel04@gmail.com>
2022-04-22 07:09:29 +0600

    m

--- node-mongo-practice/index.js
+++ node-mongo-practice/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const app = express();
 const port = process.env.port || 4000;
 
-
+app.use(cors());
 app.get('/', (req, res) => {
     res.send('running my node')
 })
