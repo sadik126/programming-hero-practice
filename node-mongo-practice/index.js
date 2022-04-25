c47b37eece939613501b3beb55a6b9979ee864fb
sadik126 <sadikhimel04@gmail.com>
2022-04-25 06:57:02 +0600

    m

--- node-mongo-practice/index.js
+++ node-mongo-practice/index.js
@@ -1,12 +1,45 @@
 const express = require('express')
 const cors = require('cors')
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.port || 4000;
 
 app.use(cors());
+app.use(express.json())
 app.get('/', (req, res) => {
-    res.send('running my node')
+    res.send('running sadik node')
 })
+
+//dbuser1
+
+//PDeBZ0RfTTaQPs5d
+
+
+
+const uri = "mongodb+srv://dbuser1:PDeBZ0RfTTaQPs5d@cluster0.jkugq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//     const collection = client.db("foodExpress").collection("users ");
+//     console.log('db connected')
+//     // perform actions on the collection object
+//     client.close();
+// });
+async function run() {
+    try {
+        await client.connect()
+        const collection = client.db("foodExpress").collection("users ");
+        const user = {
+            name: 'sakib khan',
+            email: 'sakib@gmail.com'
+        }
+    }
+    finally {
+        await client.close()
+    }
+}
+
+run().catch(console.dir)
+
 
 app.listen(port, () => {
     console.log('Crud server running')
